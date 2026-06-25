ddec6bb866e831c6d1af5d9c3278b2e55414ef9a
Copa! <ryder4est@gmail.com>
2026-06-25 13:29:35 -0700

    recommit dc cog example

---
+++ examples/discord_cog_stats.py
@@ -0,0 +1,25 @@
+import discord
+from discord.ext import commands
+
+from donutstats import DonutStats
+
+
+class Stats(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        # Initialize donutstats when your cog gets initialized
+        self.donutstats = DonutStats("Your DonutSMP api key (generate ingame with /api")
+
+    @discord.app_commands.command()
+    async def stats(self, interaction: discord.Interaction, username: str):
+        # Returns a full stats embed
+        embed: discord.Embed = await self.donutstats.get_stats_embed(username, discord.Color.blue())
+
+        await interaction.response.send_message(embed=embed)
+
+    async def cog_unload(self):
+        # Cleanly close the aiohttp connection; aiohttp gets loud about unclosed connections
+        await self.donutstats.close()
+
+
+async def setup(bot: commands.Bot):
+    await bot.add_cog(Stats(bot))
